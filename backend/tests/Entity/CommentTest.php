cfedf480621b9865558507506a631e2a802dd916
Eredost <michael.coutin62@gmail.com>
2020-08-09 15:51:18 +0200

    test: add assertions for Comment, Post and User entities

--- backend/tests/Entity/CommentTest.php
+++ backend/tests/Entity/CommentTest.php
@@ -4,7 +4,32 @@
 namespace App\Tests\Entity;
 
 
-class CommentTest
-{
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Tests\Entity\Traits\AssertTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+class CommentTest extends KernelTestCase
+{
+    use AssertTrait;
+
+    private function getEntity(): Comment
+    {
+        return (new Comment())
+            ->setMessage('Lorem ispum dolor sit amet')
+            ->setOwner(new User())
+            ->setPost(new Post())
+        ;
+    }
+
+    public function testValidEntity()
+    {
+        $this->assertHasErrors($this->getEntity(), 0);
+    }
+
+    public function testInvalidEmptyMessage()
+    {
+        $this->assertHasErrors($this->getEntity()->setMessage(''), 1);
+    }
 }
--- backend/tests/Entity/PostTest.php
+++ backend/tests/Entity/PostTest.php
@@ -4,7 +4,30 @@
 namespace App\Tests\Entity;
 
 
-class PostTest
-{
+use App\Entity\Post;
+use App\Entity\User;
+use App\Tests\Entity\Traits\AssertTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+class PostTest extends KernelTestCase
+{
+    use AssertTrait;
+
+    private function getEntity(): Post
+    {
+        return (new Post())
+            ->setContent('Lorem ispum dolor sit amet')
+            ->setOwner(new User())
+        ;
+    }
+
+    public function testValidEntity()
+    {
+        $this->assertHasErrors($this->getEntity(), 0);
+    }
+
+    public function testInvalidEmptyContent()
+    {
+        $this->assertHasErrors($this->getEntity()->setContent(''), 1);
+    }
 }
