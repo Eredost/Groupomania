a92fc236a9347f6f4e4c7a2ac9900c80f2b207db
Eredost <michael.coutin62@gmail.com>
2020-08-19 13:21:07 +0200

    feat: add Sequelize ORM dependency and mysql driver

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { Sequelize } = require('sequelize');
 
-const app = express()
+const app = express();
+
+// Sequelize connecting to the database
+const sequelize = new Sequelize(process.env.DATABASE_URI);
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully');
+    })
+    .catch(error => {
+        console.log('Unable to connect to the database : ', error);
+    })
 
 // Configuring CORS Headers
 const corsOptions = {
     origin: 'http://localhost:3001',
     allowedHeaders: 'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization'
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
 app.use('/', (req, res, next) => {
-    res.json({ msg: 'Hello world' })
+    res.send(process.env.DATABASE_URI);
 })
+
+//app.use('/api/auth');
+//app.use('/api/posts');
+//app.use('/api/comments');
 
 module.exports = app;
