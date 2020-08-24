b9037afc1da2f304d6deafb2370fe725be8ad8d4
Eredost <michael.coutin62@gmail.com>
2020-08-24 15:28:04 +0200

    feat: add signup functionality

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,16 +5,43 @@ require('dotenv').config();
 const db = require('../models');
 
 exports.signup = (req, res, next) => {
+    // Checks if the password is 8 characters long and contains a lowercase, an uppercase, a number and a special character
+    if (!/(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(req.body.password)) {
+        return res.status(400).json({ error: 'Le mot de passe doit contenir minimum 8 caractère avec au minimum un caractères minuscule et majuscule, un chiffre et un caractère spécial (!@#$%^&*)' })
+    }
 
+    // Hash the user's password
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            // Create a new User and save it to the database
+            db.User.create({
+                email: req.body.email,
+                username: req.body.username,
+                password: hash,
+            })
+                .then(user => {
+                    res.status(201).json({
+                        token: jwt.sign(
+                            { userId: user.id },
+                            process.env.JWT_SECRET_TOKEN,
+                            { expiresIn: '24h' }
+                        ),
+                    })
+                })
+                .catch(error => res.status(400).json({ error }))
+        })
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.login = (req, res, next) => {
     db.User.findOne({ where: { email: req.body.email } })
         .then(user => {
+            // Checks if the searched user exists
             if (!user) {
                 return res.status(401).json({ error: 'Informations d\'identification invalides' })
             }
 
+            // Compare the password of the recovered user with the password entered in the login form
             bcrypt.compare(req.body.password, user.password)
                 .then(isValid => {
                     if (!isValid) {
