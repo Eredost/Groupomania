9536eb834a6a76c37b49c5fba94232244c07bbc5
Eredost <michael.coutin62@gmail.com>
2020-08-24 13:38:45 +0200

    feat: add helmet and express-rate-limit dependencies

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const db = require('./models');
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/post');
 const commentRoutes = require('./routes/comment');
+
+// Setup various HTTP headers
+app.use(helmet());
+
+// Limit number of requests per 15 minutes
+const limiter = rateLimit({
+    windowMs: 1000,
+    max: 6
+});
+app.use(limiter);
+
+// Configuring CORS Headers
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    allowedHeaders: 'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization'
+};
+app.use(cors(corsOptions));
 
 // Sequelize tries to connect to the database
 db.sequelize
@@ -18,13 +37,6 @@ db.sequelize
     .catch(error => {
         console.log('Unable to connect to the database : ', error);
     })
-
-// Configuring CORS Headers
-const corsOptions = {
-    origin: 'http://localhost:3001',
-    allowedHeaders: 'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization'
-};
-app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
