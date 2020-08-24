e384ba3bce2961aff3dcd656aaf3c5d5edbe0de7
Eredost <michael.coutin62@gmail.com>
2020-08-24 09:47:55 +0200

    feat: add bcrypt dependency and hashed password in data fixtures

--- backend/seeders/20200821125832-create-user.js
+++ backend/seeders/20200821125832-create-user.js
@@ -1,26 +1,28 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', [{
       email: 'john.doe@email.com',
       username: 'John Doe',
-      roles: '["ROLE_USER"]',
-      password: 'password',
+      roles: JSON.stringify(["ROLE_USER"]),
+      password: await bcrypt.hash('password', 10),
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       email: 'john.doe2@email.com',
       username: 'John Doe2',
-      roles: '["ROLE_USER"]',
-      password: 'password',
+      roles: JSON.stringify(["ROLE_USER"]),
+      password: await bcrypt.hash('password', 10),
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       email: 'moderator@email.com',
       username: 'Moderator',
-      roles: '["ROLE_MODERATOR"]',
-      password: 'password',
+      roles: JSON.stringify(["ROLE_MODERATOR"]),
+      password: await bcrypt.hash('password', 10),
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
